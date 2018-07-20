a1b728112a99271491725c6f322294a702ef2e0e
RivemaSoft <rivemasoft@gmail.com>
2018-07-20 12:24:29 -0300

    LevaPramim
    App Courrier

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Configuração do Levapramim*/
+var krms_driver_config ={		
+	'ApiUrl':"http://databaserivemasoft.tk/levapramim/api",					
+	'DialogDefaultTitle':"LevaPramim",	
+	'PushProjectID':"levapramim-app",	
+	'APIHasKey':"AIzaSyBCLxS9QvGeY0_YE_UyAmdJ9o9EmCCq2Gk"
+};
